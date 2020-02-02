17efb2ec2d11cfa32e699c5e0ec6dfc9a1573b26
c a <chirag@gamil.com>
2020-02-02 19:06:23 +0530

    Added static files, changed logo

--- freemex/settings.py
+++ freemex/settings.py
@@ -84,28 +84,28 @@ WSGI_APPLICATION = 'freemex.wsgi.application'
 
 # if DEBUG:
 #     # Use sqlite in development
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-# else:
-    # Use MySQL in production
 DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD'),
-            'HOST': config('DB_HOST'),
-            'PORT': '',
-            #config('DB_PORT', cast=int),
-            # 'OPTIONS': {
-            #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            # },
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+# else:
+    # Use MySQL in production
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': config('DB_NAME'),
+#             'USER': config('DB_USER'),
+#             'PASSWORD': config('DB_PASSWORD'),
+#             'HOST': config('DB_HOST'),
+#             'PORT': '',
+#             #config('DB_PORT', cast=int),
+#             # 'OPTIONS': {
+#             #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#             # },
+#         }
+#     }
 
 LANGUAGE_CODE = 'en-us'
 
